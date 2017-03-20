f7c2eedd30b8ab436fb0adfdd56a21c78580ec18
Petar Santor <pero.santor@gmail.com>
2017-03-20 09:58:02 +0100

    Initial Commit

---
+++ DomZdravlja/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DomZdravlja
+//
+//  Created by Petar Santor on 3/20/17.
+//  Copyright © 2017 Petar Santor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
