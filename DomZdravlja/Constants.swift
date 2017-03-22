55a35f3573e90fe581093424f7a8d90c7931c0a6
Petar Santor <pero.santor@gmail.com>
2017-03-22 09:54:57 +0100

    Assets, constants

---
+++ DomZdravlja/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  DomZdravlja
+//
+//  Created by Petar Santor on 3/21/17.
+//  Copyright © 2017 Petar Santor. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+struct Constants {
+    struct Color {
+        static let CustomRed = UIColor(colorLiteralRed: 188/255, green: 0/255, blue: 10, alpha: 1)
+    }
+    
+    struct Path {
+        static let Documents = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        static let Tmp = NSTemporaryDirectory()
+    }
+}
