f4e6f25f60a3d1e7d6112056fa5d71d91bf0bfe9
Petar Santor <pero.santor@gmail.com>
2017-03-22 15:09:21 +0100

    Container View

---
+++ DomZdravlja/MainOptionsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainOptionsViewController.swift
+//  DomZdravlja
+//
+//  Created by Petar Santor on 3/22/17.
+//  Copyright © 2017 Petar Santor. All rights reserved.
+//
+
+import UIKit
+
+class MainOptionsViewController: UIViewController {
+    
+    @IBOutlet var buttonsOption: [UIButton]!
+    
+    override func viewDidLoad() {
+        self.setOptionButtonsAppearance()
+    }
+    
+    func setOptionButtonsAppearance() {
+        for button in self.buttonsOption {
+            button.backgroundColor = Constants.Color.CustomRed
+            button.alpha = 0.8
+            button.setTitleColor(UIColor.white, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: UIFontWeightRegular)
+        }
+    }
+    
+}
---
+++ DomZdravlja/MasterViewController.swift
@@ -0,0 +1,111 @@
+//
+//  MasterViewController.swift
+//  DomZdravlja
+//
+//  Created by Petar Santor on 3/20/17.
+//  Copyright © 2017 Petar Santor. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var buttonDrawer: UIButton!
+    
+    @IBOutlet weak var buttonEmergencyCall: UIButton!
+    
+    @IBOutlet weak var imageViewBackground: UIImageView!
+    
+    
+    //MARK: -
+    
+    private lazy var mainOptionsViewController: MainOptionsViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "idMainOptionsViewController") as! MainOptionsViewController
+        
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    private func add(asChildViewController viewController: UIViewController) {
+        // Add Child View Controller
+        addChildViewController(viewController)
+        
+        // Add Child View as Subview
+        view.addSubview(viewController.view)
+        
+        // Configure Child View
+        viewController.view.frame = CGRect(x: view.frame.origin.x,
+                                                y: view.frame.origin.y + 260,
+                                                width: view.frame.width,
+                                                height: view.frame.height - 260)
+        
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Notify Child View Controller
+        viewController.didMove(toParentViewController: self)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController) {
+        // Notify Child View Controller
+        viewController.willMove(toParentViewController: nil)
+        
+        // Remove Child View From Superview
+        viewController.view.removeFromSuperview()
+        
+        // Notify Child View Controller
+        viewController.removeFromParentViewController()
+    }
+    
+    //MARK: - Lifecycle
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupView()
+    }
+    
+    private func setupView() {
+        self.addBackgroundOverlay()
+        self.add(asChildViewController: mainOptionsViewController)
+    }
+    
+    private func addBackgroundOverlay() {
+        let overlay: UIView = UIView(frame: CGRect(x: self.imageViewBackground.frame.origin.x,
+                                                   y: self.imageViewBackground.frame.origin.y - 20,
+                                                   width: self.imageViewBackground.frame.size.width + 40,
+                                                   height: self.imageViewBackground.frame.size.height))
+        
+        overlay.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.2)
+        self.imageViewBackground.addSubview(overlay)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //MARK: - Actions
+
+    @IBAction func handleTapOnDrawerButton(_ sender: UIButton) {
+        self.remove(asChildViewController: mainOptionsViewController)
+    }
+    
+    
+    //MARK: - Utilities
+    
+    
+    
+
+}
+
