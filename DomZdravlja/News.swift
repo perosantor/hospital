18a8a1cbe33e87f4cc54c38079b5d0b4191b06d3
Petar Santor <pero.santor@gmail.com>
2017-03-27 01:40:46 +0200

    Project structure

---
+++ DomZdravlja/News.swift
@@ -0,0 +1,13 @@
+//
+//  News.swift
+//  DomZdravlja
+//
+//  Created by Petar Santor on 3/24/17.
+//  Copyright © 2017 Petar Santor. All rights reserved.
+//
+
+import UIKit
+
+class News: NSObject {
+
+}
---
+++ DomZdravlja/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  DomZdravlja
+//
+//  Created by Petar Santor on 3/24/17.
+//  Copyright © 2017 Petar Santor. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
